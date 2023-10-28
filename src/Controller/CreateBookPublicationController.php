8188eb4c20b55d03428b0fa2450cd3ff8d6902e7
huy <nguyenlehuyuit@gmail.com>
2023-10-28 17:56:11 +0700

    Add doc

---
+++ src/Controller/CreateBookPublicationController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use ApiPlatform\Api\IriConverterInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+
+class CreateBookPublicationController extends AbstractController
+{
+
+    public function __invoke()
+    {
+       dd(213);
+    }
+
+}
